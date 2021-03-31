0779458f692fe2bb3a38fc2a16935711e39791d0
shadowx897 <81704495+shadowx897@users.noreply.github.com>
2021-04-01 01:20:04 +0800

    Update category.dart

--- Models/category.dart
+++ Models/category.dart
@@ -1,4 +1,29 @@
 import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CategoryModel {
+
+  static const ID = 'id';
+  static const NAME = 'name';
+  static const IMAGE = 'image';
+
+
+  String _id;
+  String _name;
+  String _image;
+
+  //getters
+  String get id => _id;
+  String get name => _name;
+  String get image => _image;
+
+
+  CategoryModel.fromSnapshot(DocumentSnapshot snapshot){
+    _id = snapshot.data[ID];
+    _name = snapshot.data[NAME];
+    _image = snapshot.data[IMAGE];
+  }
+}
 
 class Category {
   final String name;
